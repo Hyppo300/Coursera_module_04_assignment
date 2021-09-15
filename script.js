6cc1e76e65013be65775a015835422704fed709d
Hajra_Hanif <68047400+Hyppo300@users.noreply.github.com>
2021-09-15 16:11:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-
 (function () {
-
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
 
@@ -13,5 +10,4 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker(names[i]);
   }
 }
-
 })();
